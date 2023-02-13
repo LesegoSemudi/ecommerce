5be6d39aa72743395d1bf23baba7d6114576116e
Lejay <lesego777@gmail.com>
2023-02-13 06:22:53 +0200

    update

---
+++ lib/Data/my_cart_json.dart
@@ -0,0 +1,37 @@
+const List myCartJson = [
+   {
+    "image" : "assets/images/shoes_8_.png",
+    "rate" : "4.0",
+    "name" : "Nike ISPA Link",
+    "price" : "2420,25",
+     "colors" : [
+      "assets/images/shoes_8_.png",
+      "assets/images/shoes_8_1_.png",
+    ]
+  },
+
+  {
+    "image" : "assets/images/shoes_7_.png",
+    "rate" : "4.5",
+    "name" : "Air Jordan 3 Retro",
+    "price" : "1970,21",
+     "colors" : [
+      "assets/images/shoes_7_.png",
+      "assets/images/shoes_7_1_.png",
+      "assets/images/shoes_7_2_.png",
+    ]
+  },
+   {
+    "image" : "assets/images/shoes_3_1_.png",
+    "rate" : "5.1",
+    "name" : "Air Jordan 6 Retro",
+    "price" : "1920,23",
+    "colors" : [
+      "assets/images/shoes_3_1_.png",
+      "assets/images/shoes_3_.png"
+    ]
+  },
+
+  
+  
+];
